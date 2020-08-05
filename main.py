b72bee4b37bec2e2167246b9fb8cd863ad47911b
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-08-05 23:57:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ QUERY = 'bitcoin+pric+in+inc'
 def scrape():
     r= requests.get(URL+QUERY)
     s = BeautifulSoup(r.text, 'html.parser')
-    ans = s.find('div',class_ = 'BNeawe iBp4i AP7Wnd')
+    ans = s.find('div',class_='BNeawe iBp4i AP7Wnd')
     return ans.text
 
 price = scrape()
