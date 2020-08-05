b10a1a6807d78bcdd3b719523db22219ee400273
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-08-05 23:56:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ import requests
 URl = 'https://www.google.com/search?q='
 QUERY = 'bitcoin+pric+in+inc'
 
-def scrape()
+def scrape():
+    r= requests.get(URL+QUERY)
+    s = BeautifulSoup(r.text, 'html.parser')
+    ans = s.find('div',class_ = 'BNeawe iBp4i AP7Wnd')
+    return ans.text
+
+price = scrape()
+print(f'price = {price}')
